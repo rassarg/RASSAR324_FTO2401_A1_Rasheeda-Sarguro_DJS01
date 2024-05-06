68d6945fbfb6d29a090597f7a73ddd60aa64fee8
rasheeda <raskieda@gmail.com>
2024-05-06 15:29:37 +0200

    refactor calcNewVelocity fn

--- index.js
+++ index.js
@@ -6,22 +6,26 @@
  */
 
 // Given Parameters
-const initialSpeed = 10000; // initial speed (km/h)
+const initialVelocity = 10000; // initial speed (km/h)
 const acceleration = 3; // acceleration (m/s^2)
 const timeInSeconds = 3600; // time in seconds (1 hour)
 const initialDistance = 0; // initial distance (km)
 const initialFuel = 5000; // initial fuel amount (kg)
 const fuelBurnRate = 0.5; // fuel burn rate (kg/s)
 
-const newDistance = initialDistance + initialSpeed * timeInSeconds; // calculates new distance
+const newDistance = initialDistance + initialVelocity * timeInSeconds; // calculates new distance
 const remainingFuel = fuelBurnRate * timeInSeconds; // calculates remaining fuel
-const newSpeed = calculateNewSpeed(acceleration, initialSpeed, timeInSeconds); // calculates new speed based on acceleration
+const newVelocity = calcNewVelocity(
+  acceleration,
+  initialVelocity,
+  timeInSeconds
+); // calculates new speed based on acceleration
 
 // Pick up an error with how the function below is called and make it robust to such errors
-calculateNewSpeed = (initialSpeed, acceleration, timeInSeconds) => {
-  return initialSpeed + acceleration * timeInSeconds;
-};
+function calcNewVelocity(acceleration, initialVelocity, timeInSeconds) {
+  return initialVelocity + acceleration * timeInSeconds;
+}
 
-console.log(`Corrected New Speed: ${newSpeed} km/h`);
+console.log(`Corrected New Velocity: ${newVelocity} km/h`);
 console.log(`Corrected New Distance: ${newDistance} km`);
 console.log(`Corrected Remaining Fuel: ${remainingFuel} kg`);
